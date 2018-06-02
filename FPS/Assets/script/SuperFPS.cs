e666b99ce604b03482fd5b8ee84bd9612732214a
kinhobolan <kinhobolan@live.com>
2018-06-02 11:10:10 -0300

    Initi

---
+++ FPS/Assets/script/SuperFPS.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class SuperFPS : FirstPersonController{
+
+   
+    private void Update () {
+        if (Input.GetKeyDown(KeyCode.E) ){
+
+            m_GravityMultiplier = 0;
+        }
+        if (Input.GetKeyUp (KeyCode.E)) {
+
+            m_GravityMultiplier = 3;
+        }
+
+        base.Update ();
+    }
+
+}
